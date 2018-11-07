a490432a5d06bd0856f4dcade182bbb9eebf03ab
城下秋草 <26672680@qq.com>
2018-11-07 11:08:08 +0800

    clear

--- RestApiTestingDemo/allureRun.bat
+++
@@ -1,4 +0,0 @@
-call mvn -Dtest=TestAllureReport clean test
-copy /y environment.properties .\target\allure-results
-copy /y categories.json .\target\allure-results
-allure serve .\target\allure-results
